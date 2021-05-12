cf16a60105a1ec553cbf38b55da410e403ae2e1d
Andrew <andriykokin@gmail.com>
2021-05-12 15:26:18 +0300

    Add mongoose schema

---
+++ models/Advert.js
@@ -0,0 +1,19 @@
+const { model, Schema } = require('mongoose');
+
+const schema = new Schema({
+  title: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  description: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  photoLinks: [Schema.Types.String],
+  price: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+});
+
+module.exports = model('Advert', schema);
