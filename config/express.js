2232a40c3f39ac7ab08afdaf34e9e05dc500fb5d
Andrew <andriykokin@gmail.com>
2021-05-13 15:49:14 +0300

    update

--- config/express.js
+++ config/express.js
@@ -3,8 +3,7 @@ const app = express();
 
 require('dotenv').config();
 
-app.set('port', process.env.APP_PORT || 3000);
-app.set('host', process.env.APP_HOST || 'localhost');
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.json());
 
