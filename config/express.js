963d7b3217b81857bd3c8a51a20102cb3e5d1c56
Andrew <andriykokin@gmail.com>
2021-05-14 07:40:03 +0300

    Update Package

--- config/express.js
+++ config/express.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
 require('dotenv').config();
 app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.HOST || 'localhost')
 
-const corsOptions = {
-  origin: `http://${app.get('host')}:${app.get('port')}`,
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
 
 module.exports = app;
