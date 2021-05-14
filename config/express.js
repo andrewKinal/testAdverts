a7f337e15674d48761c9cd6bc7056645fb61acae
Andrew <andriykokin@gmail.com>
2021-05-14 07:17:43 +0300

    Add cors for swagger

--- config/express.js
+++ config/express.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 require('dotenv').config();
-
 app.set('port', process.env.PORT || 3000);
 
+const corsOptions = {
+  origin: `http://localhost:${app.get('port')}`,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 module.exports = app;
